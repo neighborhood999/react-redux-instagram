600d461f4744a9913af5f3c68a320101d38b34f2
Peng-Jie <dean.leehom@gmail.com>
2016-03-20 23:42:12 +0800

    remove un use component

--- helper/handleAuth.js
+++ helper/handleAuth.js
@@ -10,11 +10,11 @@ function handleAuth(req, res) {
 
   const url = 'https://api.instagram.com/oauth/access_token';
   const form = {
-    'client_id': '52d1f7d9282a42f59e1c36f013acf974',
-    'client_secret': 'bc997656dd2f4149a70897cf67ef9464',
-    'grant_type': 'authorization_code',
-    'redirect_uri': 'http://localhost:3000/handleAuth',
-    'code': code,
+    client_id: '52d1f7d9282a42f59e1c36f013acf974',
+    client_secret: 'bc997656dd2f4149a70897cf67ef9464',
+    grant_type: 'authorization_code',
+    redirect_uri: 'http://localhost:3000/handleAuth',
+    code,
   };
   request.post({ url, form }, (err, httpResponse, body) => {
     console.log(`${httpResponse} and body: ${body}`);
