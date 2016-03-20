3730d4625b39ab8ace20bd9bccc11cbe8a38de16
Peng-Jie <dean.leehom@gmail.com>
2016-03-20 21:14:22 +0800

    rename

--- Auth/auth.js
+++
@@ -1,9 +0,0 @@
-const apiConfig = require('./apiConfig');
-
-function auth(req, res) {
-  return res.redirect(apiConfig.api.get_authorization_url(
-    apiConfig.redirectURI, { scope: ['basic', 'follower_list', 'relationships', 'likes'],
-  }));
-}
-
-module.exports = auth;
