ff083c1e737f7b09e549721b6db5b0152d15dcc2
Peng-Jie <dean.leehom@gmail.com>
2016-04-06 14:44:06 +0800

    fix file struct

--- helper/auth.js
+++
@@ -1,5 +0,0 @@
-function auth(req, res) {
-  res.redirect('https://api.instagram.com/oauth/authorize/?client_id=52d1f7d9282a42f59e1c36f013acf974&redirect_uri=http://localhost:3000/handleAuth&response_type=code&scope=likes+comments+relationships');
-}
-
-module.exports = auth;
--- src/utilitys/popup.js
+++
@@ -1,6 +0,0 @@
-const settings = 'scrollbars=no,toolbar=no,location=no,titlebar=no,directories=no,status=no,menubar=no,width=452,height=633,top=5,left=457';
-const instagramOAuthURL = 'https://api.instagram.com/oauth/authorize/?client_id=52d1f7d9282a42f59e1c36f013acf974&redirect_uri=http://localhost:3000/handleAuth&response_type=code';
-
-export default function popup() {
-  window.open(instagramOAuthURL, 'instagram', settings);
-}
